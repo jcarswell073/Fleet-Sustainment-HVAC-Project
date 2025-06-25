2eb9da8384ab1de3b1b5660fcff2711faca0c13e
jchen925 <148140603+jchen925@users.noreply.github.com>
2025-06-25 10:59:42 -0400

    Update normalization_transform.py

--- Archive/Data Cleaning/normalization_transform.py
+++ Archive/Data Cleaning/normalization_transform.py
@@ -1,12 +1,12 @@
 # Normalize price/costs
-merged['total_price_log'] = np.log(merged['total_price'] + 1)
-merged['days_open_boxcox'], _ = boxcox(merged['days_open'] + 1)
-merged['total_material_cost_log'] = np.log(merged['total_material_cost'] + 1)
-merged['total_repair_replacement_cost_log'] = np.log(merged['total_repair_replacement_cost'] + 1)
+hvacdata['total_price_log'] = np.log(hvacdata['total_price'] + 1)
+hvacdata['days_open_boxcox'], _ = boxcox(hvacdata['days_open'] + 1)
+hvacdata['total_material_cost_log'] = np.log(hvacdata['total_material_cost'] + 1)
+hvacdata['total_repair_replacement_cost_log'] = np.log(hvacdata['total_repair_replacement_cost'] + 1)
 
 
 # Plot histograms
-plt.hist(merged['total_price_log'], bins=10, edgecolor='black')
+plt.hist(hvacdata['total_price_log'], bins=10, edgecolor='black')
 plt.title('Histogram of total_price_log')
 plt.show()
 
