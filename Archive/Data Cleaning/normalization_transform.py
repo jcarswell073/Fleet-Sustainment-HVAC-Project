7d28ef5159864594b30ed1072c19cc9e12c103ff
jchen925 <148140603+jchen925@users.noreply.github.com>
2025-06-25 16:26:00 -0400

    Update normalization_transform.py

--- Archive/Data Cleaning/normalization_transform.py
+++ Archive/Data Cleaning/normalization_transform.py
@@ -4,7 +4,6 @@ from scipy.stats import boxcox
 
 
 
-# Normalize price/costs
 hvacdata['total_price_log'] = np.log(hvacdata['total_price'] + 1)
 hvacdata['days_open_boxcox'], _ = boxcox(hvacdata['days_open'] + 1)
 hvacdata['total_material_cost_log'] = np.log(hvacdata['total_material_cost'] + 1)
@@ -12,10 +11,4 @@ hvacdata['total_repair_replacement_cost_log'] = np.log(hvacdata['total_repair_re
 hvacdata['total_ship_force_man_hours_boxcox'], _ = boxcox(hvacdata['total_ship_force_man_hours'] + 1)
 hvacdata['maintenance_duration_log'] = np.log(hvacdata['maintenance_duration'] + 1)
 
-
-
-# Plot histograms
-plt.hist(hvacdata['total_price_log'], bins=10, edgecolor='black')
-plt.title('Histogram of total_price_log')
-plt.show()
 
