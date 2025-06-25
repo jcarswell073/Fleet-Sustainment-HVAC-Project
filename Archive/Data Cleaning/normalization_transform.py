c7e95e92637092a4d2f9e83ba8cd004fa088e941
jchen925 <148140603+jchen925@users.noreply.github.com>
2025-06-25 15:19:43 -0400

    Update normalization_transform.py

--- Archive/Data Cleaning/normalization_transform.py
+++ Archive/Data Cleaning/normalization_transform.py
@@ -9,6 +9,8 @@ hvacdata['total_price_log'] = np.log(hvacdata['total_price'] + 1)
 hvacdata['days_open_boxcox'], _ = boxcox(hvacdata['days_open'] + 1)
 hvacdata['total_material_cost_log'] = np.log(hvacdata['total_material_cost'] + 1)
 hvacdata['total_repair_replacement_cost_log'] = np.log(hvacdata['total_repair_replacement_cost'] + 1)
+merged1['total_ship_force_man_hours_boxcox'], _ = boxcox(merged1['total_ship_force_man_hours'] + 1)  # Adding 1 to handle zero values
+
 
 
 # Plot histograms
