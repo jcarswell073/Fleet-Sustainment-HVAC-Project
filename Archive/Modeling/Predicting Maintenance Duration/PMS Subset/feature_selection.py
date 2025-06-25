0cc7f909acb9567d38a39dd82cfaa7580ae37331
jchen925 <148140603+jchen925@users.noreply.github.com>
2025-06-25 15:32:24 -0400

    Update feature_selection.py

--- Archive/Modeling/Predicting Maintenance Duration/PMS Subset/feature_selection.py
+++ Archive/Modeling/Predicting Maintenance Duration/PMS Subset/feature_selection.py
@@ -1,3 +1,36 @@
+
+
+from scipy.stats import boxcox
+
+
+
+hvacdata['date_closing'] = pd.to_datetime(hvacdata['date_closing'], errors='coerce')
+hvacdata['date_maintenance_action'] = pd.to_datetime(hvacdata['date_maintenance_action'], errors='coerce')
+
+
+
+hvacdata['maintenance_duration'] = (
+    pd.to_datetime(hvacdata['date_closing']) - pd.to_datetime(hvacdata['date_maintenance_action'])
+).dt.days
+
+
+
+hvacdata['maintenance_duration'] = (hvacdata['date_closing'] - hvacdata['date_maintenance_action']).dt.days
+
+
+# Normalize price/costs
+hvacdata['total_price_log'] = np.log(hvacdata['total_price'] + 1)
+hvacdata['days_open_boxcox'], _ = boxcox(hvacdata['days_open'] + 1)
+hvacdata['total_material_cost_log'] = np.log(hvacdata['total_material_cost'] + 1)
+hvacdata['total_repair_replacement_cost_log'] = np.log(hvacdata['total_repair_replacement_cost'] + 1)
+hvacdata['total_ship_force_man_hours_boxcox'], _ = boxcox(hvacdata['total_ship_force_man_hours'] + 1)
+hvacdata['maintenance_duration_log'] = np.log(hvacdata['maintenance_duration'] + 1)
+
+
+# Subset data for 'PMS'
+pms_related = subset_data_by_keyword(hvacdata, 'PMS', narrative_columns)
+
+
 # target and features
 target = 'maintenance_duration_log'
 selected_features = [
